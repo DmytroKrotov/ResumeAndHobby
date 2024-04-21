0ace61543708f76329533abdf7e2a06965f07bee
dk <dk874190@gmail.com>
2024-04-21 08:15:47 +0300

    add if switch for pipes

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ResumePerson } from '../../models/resumePerson';
 import { CertificatesComponent } from '../certificates/certificates.component';
+import { CurrencyPipe, PercentPipe } from '@angular/common';
+import { NgFor,NgIf,NgSwitchCase,NgSwitch,NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CertificatesComponent],
+  imports: [CertificatesComponent,CurrencyPipe,PercentPipe,NgFor,NgIf,NgSwitchCase,NgSwitch,NgSwitchDefault],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  DominicResumeData:ResumePerson=new ResumePerson("Dominic","Toretto","../../assets/Dom_screenshot.JPG","+12131111111","dom@email.com","Los Angeles, California",["An assistant mechanic in his father's workshop","Participated in licensed car races","Illegal work for government agencies"],["raser","mechanic"],["https://en.wikipedia.org/wiki/Dominic_Toretto"]);
+  DominicResumeData:ResumePerson=new ResumePerson("Dominic","Toretto","../../assets/Dom_screenshot.JPG","+12131111111","dom@email.com","Los Angeles, California",["An assistant mechanic in his father's workshop","Participated in licensed car races","Illegal work for government agencies"],["raser","mechanic"],["https://en.wikipedia.org/wiki/Dominic_Toretto"],0.9,5000000);
 
 }
--- src/models/resumePerson.ts
+++ src/models/resumePerson.ts
@@ -2,7 +2,7 @@ import { Person } from "./person";
 
 export class ResumePerson extends Person
 {
-    constructor(name:string,lastName:string,photo:string,phone:string,email:string,address:string,experience:string[],skills:string[],links:string[])
+    constructor(name:string,lastName:string,photo:string,phone:string,email:string,address:string,experience:string[],skills:string[],links:string[],power: number,salary:number)
     {
         super(name,lastName,photo);
         this.phone=phone;
@@ -11,6 +11,8 @@ export class ResumePerson extends Person
         this.experience=experience;
         this.skills=skills;
         this.links=links;
+        this.power=power;
+        this.salary=salary;
     }
     phone:string="";
     email:string="";
@@ -18,5 +20,20 @@ export class ResumePerson extends Person
     experience:string[]=[];
     skills:string[]=[];
     links:string[]=[];
+    private _power: number=0;
+    salary:number;
+
+    get power()
+    {
+        return this._power;
+    }
+    set power(power:number)
+    {
+        if(power<=1&&power>=0)
+            this._power=power;
+        else
+            this._power= -1;
+
+    }
 
 }
